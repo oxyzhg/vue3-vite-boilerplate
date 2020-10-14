cf7dd5551c4232be794a37d5ab3b03427fb58958
Kq <oxyzhg@163.com>
2020-10-14 15:10:03 +0800

    chore: 新增项目基本配置文件

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  parser: "vue-eslint-parser",
+
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+
+  rules: {},
+};
