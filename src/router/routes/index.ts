296fb298ab1e3609a925d6cb1233c9af2a61c6f2
kq <oxyzhg@163.com>
2020-10-14 21:54:52 +0800

    fix: 修复route引入方式错误

--- src/router/routes/index.ts
+++ src/router/routes/index.ts
@@ -4,13 +4,13 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'Home',
-    component: import('../../views/Home.vue'),
+    component: () => import('../../views/Home.vue')
   },
   {
     path: '/about',
     name: 'About',
-    component: import('../../views/About.vue'),
-  },
+    component: () => import('../../views/About.vue')
+  }
 ];
 
 export default routes;
